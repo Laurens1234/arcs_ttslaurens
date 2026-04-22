de1bf3c4ed517ab707266674c903446933c4ee4a
Laurens1234 <laurensekke@gmail.com>
2026-04-22 23:46:42 +0200

    added in fated leaders setup (non special)

--- src/Global.lua
+++ src/Global.lua
@@ -3770,3 +3770,239 @@ starting_pieces["Doppelganger"] = {
     ships = 2
   }
 }
+
+
+------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+
+starting_pieces["Imperator"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Material", "Weapon"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+
+starting_pieces["Poet"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Psionic", "Weapon"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+
+starting_pieces["Diplomat"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Psionic", "Fuel"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Scavenger"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Material", "Fuel"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Oracle"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Psionic", "Material"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Brainbox"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Weapon", "Material"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Abbot"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Relic", "Relic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["God's Hand"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "city",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Psionic", "Psionic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Firebrand"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Weapon", "Psionic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Ancient Wraith"] = {
+  A = {
+    building = "None",
+    ships = 4
+  },
+  B = {
+    building = "None",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Weapon", "Psionic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Paladin"] = {
+  A = {
+    building = "None",
+    ships = 4
+  },
+  B = {
+    building = "None",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Weapon", "Psionic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
+starting_pieces["Profiteer"] = {
+  A = {
+    building = "city",
+    ships = 3
+  },
+  B = {
+    building = "starport",
+    ships = 3
+  },
+  C = {
+    building = "None",
+    ships = 3
+  },
+  resources = {"Weapon", "Psionic"},
+  D = {
+    building = "None",
+    ships = 3
+  }
+}
